47ced4c2a7ce0bda1725f84f007dc44eac5c9124
Tom Samstag <github@modtwo.com>
2019-02-12 21:54:47 -0800

    dmenu_programs: escape curly brace in perl regex

--- bin/bin/dmenu_programs.sh
+++ bin/bin/dmenu_programs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PROG="$(cat ~/.fluxbox/menu | sed 's/#.*//' | perl -ne 'print "$1\n" if(/^\s*\[exec\] \(([^)]+)\)/)' | dmenu -i)"
-EXE="$(cat ~/.fluxbox/menu | sed 's/#.*//' | perl -ne 'BEGIN { $label = shift } print "$2\n" if(/^\s*\[exec\] \(([^)]+)\) {([^}]*)}/ and $1 eq $label)' "$PROG")"
+EXE="$(cat ~/.fluxbox/menu | sed 's/#.*//' | perl -ne 'BEGIN { $label = shift } print "$2\n" if(/^\s*\[exec\] \(([^)]+)\) \{([^}]*)\}/ and $1 eq $label)' "$PROG")"
 eval $EXE
