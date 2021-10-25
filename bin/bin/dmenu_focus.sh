35a29f3904405e97b62c5957ff58c255fd9cbd10
Tom Samstag <teck@dropbox.com>
2021-10-25 14:44:54 -0700

    dmenu_focus: escape pipe characters in titles

--- bin/bin/dmenu_focus.sh
+++ bin/bin/dmenu_focus.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-title="$(wmctrl -l | cut -d' ' -f5- | sort | uniq | dmenu -i | perl -pe 's/([\[\].(){}*+])/\\$1/g')"
+title="$(wmctrl -l | cut -d' ' -f5- | sort | uniq | dmenu -i | perl -pe 's/([\[\].(){}*+|])/\\$1/g')"
 [[ ! -z "$title" ]] && focus "$title"
