a567e2c95aa3fe460972877e0f27a10e54de2a73
Tom Samstag <github@modtwo.com>
2023-01-24 22:22:01 -0800

    update fftabs to make it generic

--- bin/bin/fftabs.sh
+++ bin/bin/fftabs.sh
@@ -10,7 +10,36 @@ set -eu
 #) ' | \
 #sort | perl -e 'while(<>){ chomp; /(.*) (\d+)/; $sum += $2; printf "%5d %s\n", $2, $1 } printf "%5d\n", $sum'
 
-~/mozilla-session/lz4json/lz4jsoncat .mozilla/firefox/8vwtga7o.default/sessionstore-backups/recovery.jsonlz4 | jq -r '
+profile_file=""
+if [[ $# -eq 1 ]]
+then
+	if [[ -d "$1" ]]
+	then
+		if [[ -e "$1/recovery.jsonlz4" ]]
+		then
+			profile_file="$1/recovery.jsonlz4"
+		elif [[ -e "$1/sessionstore-backups/recovery.jsonlz4" ]]
+		then
+			profile_file="$1/sessionstore-backups/recovery.jsonlz4"
+		fi
+	elif [[ -e "$1" ]]
+	then
+		profile_file="$1"
+	else
+		if [[ -e "$HOME"/.mozilla/firefox/"$1"/sessionstore-backups/recovery.jsonlz4 ]]
+		then
+			profile_file="$HOME"/.mozilla/firefox/"$1"/sessionstore-backups/recovery.jsonlz4
+		else
+			profile_path="$(grep -A2 "Name=$1" "$HOME"/.mozilla/firefox/profiles.ini | grep Path= | sed 's#Path=##')"
+			profile_file="$HOME"/.mozilla/firefox/"$profile_path"/sessionstore-backups/recovery.jsonlz4
+		fi
+	fi
+else
+	profile_path="$(grep -B2 "Default=1" "$HOME"/.mozilla/firefox/profiles.ini | grep Path= | sed 's#Path=##')"
+	profile_file="$HOME"/.mozilla/firefox/"$profile_path"/sessionstore-backups/recovery.jsonlz4
+fi
+
+lz4jsoncat "$profile_file" | jq -r '
 .windows
 | map(.window_title = (.extData."extension:{35dd5f9a-ca89-4643-b107-f07d09cc94b5}:userWindowTitle" // "\"UNTITLED\"" | fromjson))
 | sort_by(.window_title)
